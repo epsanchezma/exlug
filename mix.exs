9b6f8e2451f3a559ae71848b7993632d8e75eb46
Guillermo Iguaran <guilleiguaran@gmail.com>
2015-05-19 17:07:39 -0500

    Add yamerl for Procfile parsing

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Exlug.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger, :httpoison]]
+    [applications: [:logger, :httpoison, :yamerl]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,7 @@ defmodule Exlug.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [{ :httpoison, "~> 0.6" }]
+    [{:httpoison, "~> 0.6"},
+     {:yamerl, github: "yakaz/yamerl"}]
   end
 end
